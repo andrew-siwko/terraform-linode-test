4d68e5447a8a2961e8a0890a065c948bc877c71b
Andrew Siwko <asiwko@siwko.org>
2026-01-28 15:48:27 -0500

    working on backend config

--- 01-provider.tf
+++ 01-provider.tf
@@ -7,18 +7,19 @@ terraform {
     }
   }
   backend "s3" {
-      bucket = "asiwko-terraform-state"
-      key = "tf/tfstate"  
-      region = "us-iad"  
-      access_key = "OBJ-ACCESS-KEY"  
-      secret_key = "OBJ-SECRET-KEY"  
-      skip_region_validation = true  
+      bucket                      = "asiwko-terraform-state"
+      key                         = "tf/tfstate"  
+      region                      = "us-iad"  
+      access_key                  = "OBJ-ACCESS-KEY"  
+      secret_key                  = "OBJ-SECRET-KEY"  
+      use_path_style              = true
+      skip_region_validation      = true  
       skip_credentials_validation = true
-      skip_requesting_account_id = true
-      skip_s3_checksum = true
-      endpoints = {
-        s3 = "https://us-iad.linodeobjects.com"
-      }
+      skip_requesting_account_id  = true
+      skip_s3_checksum            = true
+      endpoints                   = {
+                                    s3 = "https://us-iad.linodeobjects.com"
+                                  }
   }
 }
 
@@ -33,10 +34,4 @@ variable "LINODE_API_KEY" {
 provider "linode" {
   token = var.LINODE_API_KEY
 }
-
-
-# resource "azurerm_resource_group" "state-demo-secure" {
-#   name     = "state-demo"
-#   location = "eastus"
-# }
 
