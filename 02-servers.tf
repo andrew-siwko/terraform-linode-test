5ee0aaa661ecfd98856de95f647768515b160418
Andrew Siwko <asiwko@siwko.org>
2026-01-28 15:48:44 -0500

    renamed server file in line with other projects

--- 02-servers.tf
+++
@@ -1,9 +0,0 @@
-resource "linode_instance" "terraform-web" {
-        image = "linode/ubuntu24.04"
-        label = "Terraform-Web-Example"
-        group = "Terraform"
-        region = "us-east"
-        type = "g6-standard-1"
-        authorized_keys = [ "YOUR_PUBLIC_SSH_KEY" ]
-        root_pass = "YOUR_ROOT_PASSWORD"
-}
