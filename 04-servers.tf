a0d5f950e73568c6cd8f6c26321f67ba941b6f11
Andrew Siwko <asiwko@siwko.org>
2026-01-28 18:12:26 -0500

    group is depracated?

--- 04-servers.tf
+++ 04-servers.tf
@@ -1,7 +1,7 @@
 resource "linode_instance" "terraform-web" {
         image = "linode/ubuntu24.04"
         label = "Terraform-Web-Example"
-        group = "Terraform"
+        # group = "Terraform"
         region = "us-east"
         type = "g6-standard-1"
         authorized_keys = [ "YOUR_PUBLIC_SSH_KEY" ]
