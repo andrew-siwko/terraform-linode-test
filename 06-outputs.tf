cc34fecabcb71d9bec47a156e52d4c0c48ef940e
Andrew Siwko <asiwko@siwko.org>
2026-01-29 11:18:03 -0500

    explore output values

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -1,4 +1,12 @@
-output "machine" {
-  value = linode_instance.asiwko-vm-01.*
+output "machine_keys" {
+  value = keys(linode_instance.asiwko-vm-01)
 }
 
+output "machine_info" {
+  value = {
+    id     = linode_instance.asiwko-vm-01.id
+    label  = linode_instance.asiwko-vm-01.label
+    ip     = linode_instance.asiwko-vm-01.ip_address
+    region = linode_instance.asiwko-vm-01.region
+  }
+}
