301e262613c2e702ae27c27ea00d80a9c33fa337
Andrew Siwko <asiwko@siwko.org>
2026-01-29 11:35:31 -0500

    switch to ipv4 and ipv6

--- 06-outputs.tf
+++ 06-outputs.tf
@@ -6,7 +6,8 @@ output "machine_info" {
   value = {
     id     = linode_instance.asiwko-vm-01.id
     label  = linode_instance.asiwko-vm-01.label
-    ip     = linode_instance.asiwko-vm-01.ip_address
+    ipv4   = linode_instance.asiwko-vm-01.ipv4
+    ipv6   = linode_instance.asiwko-vm-01.ipv6
     region = linode_instance.asiwko-vm-01.region
   }
 }
