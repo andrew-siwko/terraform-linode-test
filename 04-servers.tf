d906f6d3408694f8abf159fde914cedb57944197
Andrew Siwko <asiwko@siwko.org>
2026-01-29 10:31:33 -0500

    update plan and region; add key ; remove password

--- 04-servers.tf
+++ 04-servers.tf
@@ -2,8 +2,8 @@ resource "linode_instance" "terraform-web" {
   image = "linode/ubuntu24.04"
   label = "Terraform-Web-Example"
   # group = "Terraform"
-  region          = "us-east"
-  type            = "g6-standard-1"
-  authorized_keys = ["YOUR_PUBLIC_SSH_KEY"]
-  root_pass       = "YOUR_ROOT_PASSWORD"
+  region          = "us-iad"
+  type            = "g6-nanode-1"
+  authorized_keys = [file("/container_shared/ansible/ansible_rsa.pub")]
+#   root_pass       = "YOUR_ROOT_PASSWORD"
 }
