5e0889dfc5eb932e29eb045ba926c484e5571949
Andrew Siwko <asiwko@siwko.org>
2026-01-29 11:11:10 -0500

    add domain zone

---
+++ 05-domain.tf
@@ -0,0 +1,23 @@
+# use this once to get the zone into the state file
+# terraform import linode_domain.siwko_org 1228113
+
+# This will update the dns records in my siwko.org domain for the new instances.
+resource "linode_domain" "siwko_org" {
+    type = "master"
+    domain = "siwko.org"
+    soa_email = "asiwko@siwko.org"
+    refresh_sec = 30
+    retry_sec   = 30
+    ttl_sec     = 30
+}
+
+# Records for the public IP addresses.
+# resource "linode_domain_record" "gcp01_siwko_org" {
+#     domain_id = linode_domain.siwko_org.id
+#     name = "lin01"
+#     record_type = "A"
+#     ttl_sec = 5
+#     target = ???
+# }
+
+
