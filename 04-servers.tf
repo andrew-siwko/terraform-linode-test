d08400ce80d9e4b7e6a9bd7060ccb4f7113b3c26
Andrew Siwko <asiwko@siwko.org>
2026-01-29 11:03:39 -0500

    try another region, iad is full

--- 04-servers.tf
+++ 04-servers.tf
@@ -1,9 +1,10 @@
 resource "linode_instance" "terraform-web" {
   image = "linode/almalinux9"
-#   image = "linode/rocky9"
+  # image = "linode/rocky9"
   label = "asiwko-vm-01"
-  # group = "Terraform"
-  region          = "us-iad"
+  # iad was full
+  # region          = "us-iad"
+  region          = "us-southeast"
   type            = "g6-nanode-1"
   authorized_keys = [trimspace(file("/container_shared/ansible/ansible_rsa.pub"))]
   root_pass       = trimspace(file("/container_shared/ansible/linode-root-pw.txt"))
