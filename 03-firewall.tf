c5c213dac5f6c097bf0a1dab4088da250c645a81
Andrew Siwko <asiwko@siwko.org>
2026-01-30 19:12:50 -0500

    add a firewall

---
+++ 03-firewall.tf
@@ -0,0 +1,45 @@
+resource "linode_firewall" "asiwko_firewall" {
+  label = "asiwko_firewall"
+
+  inbound {
+    label    = "allow-ssh"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "22"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+
+  inbound {
+    label    = "allow-http"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "80"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+
+  inbound {
+    label    = "allow-https"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "443"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+
+  inbound {
+    label    = "allow-qha"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "1781"
+    ipv4     = ["0.0.0.0/0"]
+    ipv6     = ["::/0"]
+  }
+
+  inbound_policy = "DROP"
+
+  outbound_policy = "ACCEPT"
+
+  linodes = [linode_instance.asiwko-vm-01.id, linode_instance.asiwko-vm-02.id]
+}
