6368fa03bad02bf89f6037b2eab38362cea13f6e
Andrew Siwko <asiwko@siwko.org>
2026-02-05 17:07:28 -0500

    only one ipv4

--- 07-outputs.tf
+++ 07-outputs.tf
@@ -2,7 +2,7 @@ output "machine_info-01" {
   value = {
     id     = linode_instance.asiwko-vm-01.id
     label  = linode_instance.asiwko-vm-01.label
-    ipv4   = linode_instance.asiwko-vm-01.ipv4
+    ipv4   = one(linode_instance.asiwko-vm-01.ipv4)
     ipv6   = linode_instance.asiwko-vm-01.ipv6
     region = linode_instance.asiwko-vm-01.region
   }
