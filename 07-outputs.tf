4eafd55b53b46bc29132f7beaa7494b974108004
Andrew Siwko <asiwko@siwko.org>
2026-02-10 22:09:11 -0500

    standardize outputs

--- 07-outputs.tf
+++ 07-outputs.tf
@@ -1,9 +1,13 @@
-output "machine_info-01" {
-  value = {
-    id     = linode_instance.asiwko-vm-01.id
-    label  = linode_instance.asiwko-vm-01.label
-    ipv4   = one(linode_instance.asiwko-vm-01.ipv4)
-    ipv6   = linode_instance.asiwko-vm-01.ipv6
-    region = linode_instance.asiwko-vm-01.region
-  }
+output "linode_instance_public_ip" {
+  value   = one(linode_instance.asiwko-vm-01.ipv4)
 }
+
+# output "machine_info-01" {
+#   value = {
+#     id     = linode_instance.asiwko-vm-01.id
+#     label  = linode_instance.asiwko-vm-01.label
+#     ipv4   = one(linode_instance.asiwko-vm-01.ipv4)
+#     ipv6   = linode_instance.asiwko-vm-01.ipv6
+#     region = linode_instance.asiwko-vm-01.region
+#   }
+# }
