9fde6a50e17f26f4c5121060422c7338bb4222d0
Andrew Siwko <asiwko@siwko.org>
2026-03-13 12:12:56 -0400

    move state to local storage

--- 02-providers.tf
+++ 02-providers.tf
@@ -7,21 +7,27 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "asiwko-terraform-state"
-    key    = "tf/tfstate"
-    region = "us-iad-1"
-    endpoints = {
-      s3 = "https://us-iad-10.linodeobjects.com"
-    }
-
-    use_path_style              = true
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-
+  backend "local" {
+    path = "/container_shared/tfstate/linode.tfstate"
   }
+
+  # This project started with the state stored in the provider's oject storage.  
+  # I moved it to local storage as providers charge for object storage and there was no benefit once the exercise was complete.
+  # backend "s3" {
+  #   bucket = "asiwko-terraform-state"
+  #   key    = "tf/tfstate"
+  #   region = "us-iad-1"
+  #   endpoints = {
+  #     s3 = "https://us-iad-10.linodeobjects.com"
+  #   }
+
+  #   use_path_style              = true
+  #   skip_credentials_validation = true
+  #   skip_region_validation      = true
+  #   skip_requesting_account_id  = true
+  #   skip_s3_checksum            = true
+
+  # }
 }
 provider "azurerm" {
   features {}
